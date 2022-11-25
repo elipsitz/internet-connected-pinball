bc54072183985f15b6058964f6790d807b8f8090
Eli Lipsitz <eli.lipsitz@gmail.com>
2022-11-25 12:45:26 -0600

    controller-v3: add debug /gpio endpoint

--- software/controller-v3/src/ui.cpp
+++ software/controller-v3/src/ui.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <HTTPUpdateServer.h>
 #include <WebServer.h>
+#include <hardware/gpio.h>
 
 #include "bus.h"
 
@@ -19,6 +20,23 @@ void ui_setup()
         String response = "total count: ";
         response += bus_count;
         web_server.send(200, "text/plain", response);
+    });
+    web_server.on("/gpio", []() {
+        uint32_t gpio = gpio_get_all();
+        String response = "<pre>raw: ";
+        response += gpio;
+        response += "\n";
+        const char* gpio_names[] = {"D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "CL", "WR", "OE", "__", "__", "__", "__", "__", "__", "__", "__", "__"};
+        for (int i = 0; i < 30; i++) {
+            response += "GPIO ";
+            response += i;
+            response += " (";
+            response += gpio_names[i];
+            response += "): ";
+            response += (gpio >> i) & 1;
+            response += "\n";
+        }
+        web_server.send(200, "text/html", response);
     });
     http_updater.setup(&web_server);
     web_server.begin();
